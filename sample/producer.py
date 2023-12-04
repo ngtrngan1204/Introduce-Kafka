d7bc0e13d80d598af4224054e1d3c97b3b1c1ef3
Trà Ngân <109300791+ngtrngan1204@users.noreply.github.com>
2023-12-04 21:52:23 +0700

    Create producer.py

---
+++ sample/producer.py
@@ -0,0 +1,25 @@
+from confluent_kafka import Producer
+
+# Kafka broker details
+bootstrap_servers = '118.68.13.3:8098'
+topic = 'logger-testing'
+
+# Kafka producer configuration with basic authentication
+conf = {
+    'bootstrap.servers': bootstrap_servers,
+}
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f'Message delivery failed: {err}')
+    else:
+        print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+
+# Create Kafka producer
+producer = Producer(conf)
+
+# Produce a message to the topic
+producer.produce(topic, key='key', value='your_message', callback=delivery_report)
+
+# Wait for any outstanding messages to be delivered and delivery report callbacks to be triggered
+producer.flush()
