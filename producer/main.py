b490b479c964aaf03763d0819a20a832df0de4d6
Trà Ngân <109300791+ngtrngan1204@users.noreply.github.com>
2023-12-07 16:43:09 +0700

    Update main.py

--- producer/main.py
+++ producer/main.py
@@ -5,40 +5,54 @@ import redis
 
 app = Flask(__name__)
 
-redis_client = redis.StrictRedis(host='my-redis', port=6379, decode_responses=True)
+# Kết nối tới Redis
+r = redis.StrictRedis(host='localhost', port=6379, decode_responses=True)
 
-# Kafka configuration
+# Cấu hình Kafka
 bootstrap_servers = '118.68.13.3:8099'
 topic = 'logger-testing'
 
-# Create Kafka Producer
+
+# Tạo Kafka Producer
 producer_conf = {'bootstrap.servers': bootstrap_servers}
 kafka_producer = Producer(producer_conf)
 
 def delivery_report(err, msg):
     if err is not None:
-        print(f'Message delivery failed: {err}')
-        # Log the error, handle retries or other actions here
+        print(f'Message delivery failed: {err.code()}')
+        # Ghi log lỗi, xử lý thử lại hoặc các hành động khác
     else:
         print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
 
-# Endpoint to handle incoming data from devices via curl
-@app.route('/', methods=['POST'])
-def receive_data():
-    data = json.loads(request.data.decode('utf-8'))
-    id_value = data.get('id')
+# Endpoint nhận dữ liệu POST từ client rồi gửi đến Kafka
+@app.route('/cpu-info/<id_counter>', methods=['POST'])
+def receive_data(id_counter):
+    data = json.loads(request.data)
+    id=str(id_counter)
+    raw=json.dumps({
+            "total": data.get("total"),
+            "used": data.get("used"),
+            "free": data.get("free"),
+            "shared": data.get("shared"),
+            "buff/cache": data.get("buff/cache"),
+            "available": data.get("available")
+        })
 
-    # Push data to Kafka
-    kafka_producer.produce(topic, key='id', value=id_value, callback=delivery_report)
+    # Gửi dữ liệu tới Kafka
+    kafka_producer.produce(topic, key=id, value=raw, callback=delivery_report)   
+
     kafka_producer.flush()
 
-    return jsonify(data)
+    return ({id : json.loads(raw)}) # Trả về dữ liệu dưới dạng JSON
 
-@app.route('/', methods=['GET'])
-def get_data():
-    # Retrieve data from Redis
-    data = redis_client.get('id')
-    return jsonify({'id':data})
+# Endpoint truy cập dữ liệu từ Redis 
+@app.route('/cpu-info/<m_id>', methods=['GET'])
+def get_data(m_id):
+    data = r.get(m_id)
+    if data:
+        return jsonify({m_id : json.loads(data)}) # Nếu key đúng thì trả về dữ liệu từ Redis
+    else:
+        return 'Data not found!!' # Nếu key sai thì thông báo Data not found
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5050)
