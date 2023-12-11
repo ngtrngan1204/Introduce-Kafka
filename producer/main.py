9c7f1045bb78a2a21876a131406c9be388bff6c0
Trà Ngân <109300791+ngtrngan1204@users.noreply.github.com>
2023-12-11 15:51:26 +0700

    Update main.py

--- producer/main.py
+++ producer/main.py
@@ -2,15 +2,27 @@ from flask import Flask, jsonify, request
 from confluent_kafka import Producer
 import json
 import redis
+import os
+import logging
 
 app = Flask(__name__)
 
+# Set up file log
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('error.log'),
+        logging.StreamHandler()
+    ]
+)
+
 # Kết nối tới Redis
-r = redis.StrictRedis(host='localhost', port=6379, decode_responses=True)
+r = redis.StrictRedis(host='my-redis', port=6379, decode_responses=True)
 
 # Cấu hình Kafka
-bootstrap_servers = '118.68.13.3:8099'
-topic = 'logger-testing'
+bootstrap_servers = os.environ['SERVER']
+topic = os.environ['TOPIC']
 
 
 # Tạo Kafka Producer
@@ -22,28 +34,46 @@ def delivery_report(err, msg):
         print(f'Message delivery failed: {err.code()}')
         # Ghi log lỗi, xử lý thử lại hoặc các hành động khác
     else:
-        print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+        logging.error(f'Message delivered to {msg.topic()} [{msg.partition()}]')
 
 # Endpoint nhận dữ liệu POST từ client rồi gửi đến Kafka
 @app.route('/cpu-info/<id_counter>', methods=['POST'])
 def receive_data(id_counter):
-    data = json.loads(request.data)
-    id=str(id_counter)
-    raw=json.dumps({
-            "total": data.get("total"),
-            "used": data.get("used"),
-            "free": data.get("free"),
-            "shared": data.get("shared"),
-            "buff/cache": data.get("buff/cache"),
-            "available": data.get("available")
-        })
+    try:
+        data = json.loads(request.data)
 
-    # Gửi dữ liệu tới Kafka
-    kafka_producer.produce(topic, key=id, value=raw, callback=delivery_report)   
+        # Kiểm tra xem dữ liệu gửi lên có đúng định dạng và không rỗng
+        required_fields = ["total", "used", "free", "shared", "buff/cache", "available"]
+        for field in required_fields:
+            if field not in data or not isinstance(data[field], int):
+                if field not in data:
+                    response = jsonify({"error": f"Missing field: {field}"})
+                else:
+                    response = jsonify({"error": f"Invalid field type for '{field}' - must be an integer"})
+                response.status_code = 400  # Return status code 400 - Bad Request
+                return response
+        
+        for field in data:
+            if field not in required_fields:
+                response = jsonify({"error": f"Extra field not allowed: '{field}'"})
+                response.status_code = 400  # Return status code 400 - Bad Request
+                return response
+    
 
-    kafka_producer.flush()
+        id = str(id_counter)
+        raw = json.dumps(data)
 
-    return ({id : json.loads(raw)}) # Trả về dữ liệu dưới dạng JSON
+        # Gửi dữ liệu tới Kafka
+        kafka_producer.produce(topic, key=id, value=raw, callback=delivery_report)
+        kafka_producer.flush()
+
+        return jsonify({id: data})  # Trả về dữ liệu gửi lên dưới dạng JSON
+
+    except json.JSONDecodeError:
+        response = jsonify({"error": "Invalid JSON format"})
+        response.status_code = 400  # Trả về status code 400 - Bad Request
+        return response
+
 
 # Endpoint truy cập dữ liệu từ Redis 
 @app.route('/cpu-info/<m_id>', methods=['GET'])
